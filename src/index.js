ae4b14f7e96116cc56f37aa59bd3984093587b48
alex1393 <jjbumers@gmail.com>
2023-01-07 20:24:59 -0500

    add fetch start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const getResource = async (url) => {
+  const res = await fetch(url);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const body = await res.json();
+  return body;
+};
+
+getResource("https://swapi.dev/api/people/123123")
+  .then((body) => {
+    console.log(body);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
