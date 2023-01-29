dc936d0206ffc644d363f5b881ecd5beef326c92
alex1393 <jjbumers@gmail.com>
2023-01-28 22:41:15 -0500

    fix component

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -2,25 +2,25 @@ import React, { Component } from "react";
 import "./item-list.css";
 import SwapiService from "../../services/swapi-service";
 import withData from "../hoc-helpers/with-data";
+
 const ItemList = (props) => {
   const { data, onItemSelected, children: renderLabel } = props;
 
   const items = data.map((item) => {
     const { id } = item;
     const label = renderLabel(item);
+    return (
+      <li
+        className="list-group-item"
+        key={id}
+        onClick={() => {
+          onItemSelected(id);
+        }}
+      >
+        {label}
+      </li>
+    );
   });
-
-  return (
-    <li
-      className="list-group-item"
-      key={id}
-      onClick={() => {
-        onItemSelected(id);
-      }}
-    >
-      {label}
-    </li>
-  );
 
   return <ul className="item-list list-group">{items}</ul>;
 };
